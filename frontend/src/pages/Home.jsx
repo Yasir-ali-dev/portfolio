a14e1f4ba3d6307a5db843e983a2cc16c3538a67
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2023-09-27 19:12:46 +0500

    Home page completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import {Box, Button, ButtonGroup, Stack, Typography} from '@mui/material';
-import {ArrowOutward, Email, Facebook, GitHub, LinkedIn, LocalPhone} from '@mui/icons-material';
+import {Box, Button, Stack, Typography} from '@mui/material';
+import {ArrowOutward, Email, EmailOutlined, Facebook, GitHub, Instagram, LinkedIn, LocalPhone} from '@mui/icons-material';
 const links= ["Home","About","Services","Work","Contact"];
 
 const Home = () => {
   return (
     <div>
-      <Stack display={"flex"} flexDirection={"row"} height={"100vh"}>
+      <Stack display={"flex"} flexDirection={"row"} >
         <Box 
             flex={1.5} 
             sx={{
@@ -15,24 +15,24 @@ const Home = () => {
             }} 
             display={"flex"} 
             flexDirection={"column"}
-            padding={"1em 1em"}
+            padding={"0em 1em"}
             justifyContent={"space-around"}
         >
             <Box
                 sx={{
-                    padding:"1em .9em "
+                    padding:".5em .9em "
                 }} 
             >
             
-                <Typography variant='h4'>Yasir.</Typography>
+                <Typography variant='h3'>Yasir.</Typography>
             </Box>
           
             <Box 
                 sx={{
                     display:"flex",
                     flexDirection:"column", 
-                    gap:"1em",
-                    padding:"1em 1em "
+                    gap:".5em",
+                    padding:".3em 1em "
                 }} >
             
                  {links.map((link , index) =>{
@@ -44,8 +44,8 @@ const Home = () => {
                 display={"flex"} 
                 flexDirection={"column" } 
                 sx={{
-                    gap:".7em",
-                    padding:".5em 1em "
+                    gap:".5em",
+                    padding:".3em 1em "
                 }}
             >    
                 <GitHub fontSize='large'/>
@@ -62,9 +62,9 @@ const Home = () => {
         >    
           <Stack
             flex={1}
-            padding={"6em 2em"}
-            gap={"1.2em"}
-            spacing={".45em"}
+            padding={"5em 2em"}
+            gap={"1em"}
+            spacing={".55em"}
           >
             <Typography variant='h2'>
                 MY NAME IS <strong>YASIR ALI...</strong>
@@ -72,22 +72,48 @@ const Home = () => {
             <Typography variant='h3' >
                 <strong>Web Developer</strong> with expertise in <strong>MERN</strong>
             </Typography>
-            <Button>
+            <Button variant='contained'  
+                sx={{
+                    width:"17em", backgroundColor:'#141313',
+                    padding:"1.15em 0"
+                }}>
                 let's talk with me <ArrowOutward/> 
             </Button>
-             <Box display={"flex"} alignItems={"center"} gap={1}>
-                <LocalPhone fontSize='large'/>  0000-1222-1111 
-                <Email fontSize='large'/> yasir@gmail.com  
+             <Box 
+                display={"flex"} 
+                padding={".94em 0em"}
+                gap={1}
+             >   
+                <Box 
+                    display={"flex"} 
+                    alignItems={"center"}
+                >
+                    <LocalPhone fontSize='large'/>  0000-1222-1111 
+                </Box>
+                <Box 
+                    display={"flex"} 
+                    alignItems={"center"}
+                >
+                    <Email fontSize='large'/> yasir@gmail.com  
+                </Box>
              </Box>       
            </Stack> 
-       
           <Box 
              flex={1}
+             padding={"1.2em 2em"}
+             width={"100%"}
           > 
-            </Box> 
-               
-
-
+            <img src={require("../assets/Img.png")} alt="" width={"100%"} />
+          </Box> 
+          <Box
+            display={"flex"}
+            flexDirection={"column"}
+            justifyContent={"center"}    
+           >
+            <Facebook color='primary' fontSize='large'/>
+            <Instagram fontSize='large'/>
+            <EmailOutlined fontSize='large'/>   
+          </Box>
         </Box>
       </Stack>
     </div>
