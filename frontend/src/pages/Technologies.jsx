5f3f3be54af23aaff2b878506de2a82da94e21cf
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2023-10-05 22:40:09 +0500

    Technologies section fixed in front end

--- frontend/src/pages/Technologies.jsx
+++ frontend/src/pages/Technologies.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Box, Stack } from '@mui/material';
 
+const tech=["/R.svg","/node-js.svg","/mongodb.svg","/express.svg","/JScript.svg","/java.svg","/git.svg","postman.svg"];
 const Technologies = () => {
   return (
     <Stack 
@@ -14,23 +15,26 @@ const Technologies = () => {
       alignItems={"center"}
       flexWrap={"wrap"}
       >
+          {
+            tech.map((_,index)=>{
+              return (
+                <Box width={"90px"}>
+                 <img 
+                    key={index}
+                    src={_} 
+                    alt="nodejs-logo"
+                    width={"100%"}
+                 />
+                </Box>
+                )  
+            })
+          }
+        {/* <Box   >
+          
+        </Box> 
         <Box width={"90px"} >
           <img 
-            src={require("../assets/react.svg")} 
-            alt="react-logo"
-            width={"100%"}
-             />
-        </Box> 
-        <Box  width={"90px"} >
-          <img 
-            src={require("../assets/node-js.svg")} 
-            alt="nodejs-logo"
-            width={"100%"}
-             />
-        </Box> 
-        <Box width={"90px"} >
-          <img 
-            src={require("../assets/java.svg")} 
+            src={"/node-js.svg"} 
             alt="express-logo"
             width={"100%"}
              />
@@ -39,7 +43,7 @@ const Technologies = () => {
           <img 
             width={"100%"}
             alignSelf={"center"}
-            src={require("../assets/mongodb.svg")} 
+            src={"/mongodb.svg"} 
             alt="mongodb"
             
              />
@@ -47,11 +51,12 @@ const Technologies = () => {
         <Box width={"90px"}>
           <img
             alignSelf={"center"} 
-            src={require("../assets/JScript.svg")} 
+            src={"/jScript.svg"} 
             alt="JS-logo"
             width={"100%"}
              />
-        </Box>   
+        </Box> */}
+
     </Stack>
   )
 }
